74d03b6179d418afa8e387f91aaee9bf85d4c7ff
Anand Rawool <anandrawool5624@gmail.com>
2025-04-29 01:56:22 +0530

    add new

---
+++ src/SolidPrinciple/L/Bird.java
@@ -0,0 +1,45 @@
+package SolidPrinciple.L;
+
+// L: subclass should be extended the capability of parent class not narrow it down
+
+public class Bird {
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+}
+
+class Sparrow extends Bird {
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+}
+
+class Ostrich extends Bird {
+    public void fly() {
+        // Ostriches can't fly, but not throwing an exception so here break the properties of parent
+        throw new UnsupportedOperationException("Ostriches can't fly");
+    }
+}
+
+//solution:
+// Base class
+abstract class Bird1{
+    public abstract void fly();
+}
+
+// Derived class
+class Sparrow1 extends Bird {
+    @Override
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+}
+
+// Derived class
+class Ostrich1 extends Bird {
+    @Override
+    public void fly() {
+        // Ostriches can't fly, but not throwing an exception
+        System.out.println("Ostrich can't fly");
+    }
+}
